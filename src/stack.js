3de4c069bc1460d230da7775e621135b121466c7
Mark Kulishov <>
2022-10-16 12:26:15 +0300

    Stack

--- src/stack.js
+++ src/stack.js
@@ -1,4 +1,5 @@
 const { NotImplementedError } = require('../extensions/index.js');
+const {assert} = require("chai");
 
 /**
  * Implement the Stack with a given interface via array.
@@ -13,19 +14,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.arr = [];
+  }
+
+  push(element) {
+    this.arr.push(element);
+
+    return this;
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.arr.pop();
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.arr.slice(-1)[0]
   }
 }
 
